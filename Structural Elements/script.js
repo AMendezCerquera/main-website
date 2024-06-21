9a43aa2b4deead5b3555529ac1623d896e362623
AMendezCerquera <76956375+AMendezCerquera@users.noreply.github.com>
2024-06-21 18:43:51 -0400

    Updated design to one page
    Missing:
    - Update the chatbox.
    - Update the logo.
    - Update sections.

--- Structural Elements/script.js
+++ Structural Elements/script.js
@@ -1,111 +1,138 @@
 async function navigateLoad() {
-    try {
-        // Await the fetch operation to complete and then convert response to text
-        const response = await fetch("/Structural Elements/Loaded Content/Navigation/nav.html");
-        const socialBar = await fetch("/Structural Elements/Loaded Content/Social Bar/index.html");
-        const footerBar = await fetch("/Structural Elements/Loaded Content/Footer/footer.html");
-        const chatBox = await fetch("/Structural Elements/Loaded Content/Chatbox/index.html");
+  try {
+    // Await the fetch operation to complete and then convert response to text
+    const response = await fetch(
+      "/Structural Elements/Loaded Content/Navigation/nav.html"
+    );
+    const footerBar = await fetch(
+      "/Structural Elements/Loaded Content/Footer/footer.html"
+    );
+    const chatBox = await fetch(
+      "/Structural Elements/Loaded Content/Chatbox/index.html"
+    );
 
-        if (!response.ok || !socialBar.ok || !footerBar.ok || !chatBox.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.text();
-        const socialData = await socialBar.text();
-        const footerData = await footerBar.text();
-        const chatData = await chatBox.text();
-
-        // Replace the placeholder with the fetched navigation content
-        document.getElementById("nav-placeholder").outerHTML = data;
-        document.getElementById("social-bar-placeholder").outerHTML = socialData;
-        document.getElementById("footer-placeholder").outerHTML = footerData;
-        document.getElementById("chatbot-placeholder").outerHTML = chatData;
-
-        // Access the elements after they have been added to the DOM
-        navigateFunctionalities();
-
-        const swiper = new Swiper(".mySwiper", {
-            direction: "vertical",
-            slidesPerView: 1,
-            spaceBetween: 0,
-            mousewheel: true,
-            pagination: {
-              el: ".swiper-pagination",
-              clickable: true,
-            },
-            autoplay: {
-                delay: 10000,
-            },
-          });
-        
-
-    } catch (error) {
-        console.error('Failed to load navigation:', error);
+    if (!response.ok || !footerBar.ok || !chatBox.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+    const data = await response.text();
+    const footerData = await footerBar.text();
+    const chatData = await chatBox.text();
+
+    // Replace the placeholder with the fetched navigation content
+    document.getElementById("nav-placeholder").outerHTML = data;
+    document.getElementById("footer-placeholder").outerHTML = footerData;
+    document.getElementById("chatbot-placeholder").outerHTML = chatData;
+
+    // Access the elements after they have been added to the DOM
+    navigateFunctionalities();
+
+    const swiper = new Swiper(".mySwiper", {
+      direction: "vertical",
+      slidesPerView: 1,
+      spaceBetween: 0,
+      mousewheel: false,
+      autoHeight: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+        delay: 10000,
+      },
+    });
+  } catch (error) {
+    console.error("Failed to load navigation:", error);
+  }
 }
 
 function navigateFunctionalities() {
-    const navMenu = document.getElementById('nav-menu');
-    const navToggle = document.getElementById('nav-toggle');
-    const navClose = document.getElementById('nav-close');
+  const navMenu = document.getElementById("nav-menu");
+  const navList = document.getElementById("nav-list");
+  const navToggle = document.getElementById("nav-toggle");
+  const navClose = document.getElementById("nav-close");
 
-    //Menu Show
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-    });
+  //Menu Show
+  navToggle.addEventListener("click", () => {
+    navList.classList.toggle("expanded");
+    navToggle.style.display = "none";
+    navClose.style.display = "flex";
+  });
 
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    });
+  navClose.addEventListener("click", () => {
+    navList.classList.toggle("expanded");
+    navClose.style.display = "none";
+    navToggle.style.display = "flex";
+  });
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const chatbot = document.getElementById('chatbot');
-        chatbot.classList.add('hidden'); // Ensure chatbot is hidden on page load
-    });
-    
-    const input = document.getElementById('chat-input');
-    input.addEventListener("keypress", function(event) {
+  document.addEventListener("DOMContentLoaded", function () {
+    const chatbot = document.getElementById("chatbot");
+    chatbot.classList.add("hidden"); // Ensure chatbot is hidden on page load
+  });
+
+  const input = document.getElementById("chat-input");
+  input.addEventListener("keypress", function (event) {
     // If the user presses the "Enter" key on the keyboard
-        if (event.key === "Enter") {
+    if (event.key === "Enter") {
       // Cancel the default action, if needed
-        event.preventDefault();
+      event.preventDefault();
       // Trigger the button element with a click
-        sendMessage();
-        }
-    });
+      sendMessage();
+    }
+  });
 }
 
 function toggleChat() {
-        const chatbot = document.getElementById('chatbot');
-        chatbot.classList.toggle('hidden');
+  const chatbot = document.getElementById("chatbot");
+  chatbot.classList.toggle("hidden");
 }
-
 
 function sendMessage() {
-    const input = document.getElementById('chat-input');
-    const output = document.getElementById('chat-output');
+  const input = document.getElementById("chat-input");
+  const output = document.getElementById("chat-output");
 
-    const message = input.value.trim();
-    if (message !== '') {
-        output.innerHTML += `<div class='user-message'>${message}</div>`;
+  const message = input.value.trim();
+  if (message !== "") {
+    output.innerHTML += `<div class='user-message'>${message}</div>`;
 
-        const formData = new URLSearchParams();
-        formData.append('question', message);
+    const formData = new URLSearchParams();
+    formData.append("question", message);
 
-        fetch('http://localhost:3000/faq', {
-            method: 'POST',
-            body: formData,
-        })
-        .then(response => response.json())
-        .then(data => {
-            output.innerHTML += `<div class='bot-response'>${data.answer}</div>`;
-            output.scrollTop = output.scrollHeight; // Scroll to the bottom
-        })
-        .catch(error => console.error('Error:', error));
+    fetch("http://localhost:3000/faq", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        output.innerHTML += `<div class='bot-response'>${data.answer}</div>`;
+        output.scrollTop = output.scrollHeight; // Scroll to the bottom
+      })
+      .catch((error) => console.error("Error:", error));
 
-        input.value = ''; // Clear input after sending
-    }
+    input.value = ""; // Clear input after sending
+  }
 }
 
+var resizeLeft = function resizeLeft() {
+  var r = document.getElementById("right-col");
+  var l = document.getElementById("left-col");
+
+  l.style.height = r.offsetHeight + "px";
+  console.log(r.offsetHeight);
+};
+
+var addEvent = function (elem, type, eventHandle) {
+  if (elem == null || typeof elem == "undefined") return;
+  if (elem.addEventListener) {
+    elem.addEventListener(type, eventHandle, false);
+  } else if (elem.attachEvent) {
+    elem.attachEvent("on" + type, eventHandle);
+  } else {
+    elem["on" + type] = eventHandle;
+  }
+};
+addEvent(window, "resize", resizeLeft);
+
+resizeLeft();
 
 // Call the navigateLoad function on window load
 window.onload = navigateLoad();
