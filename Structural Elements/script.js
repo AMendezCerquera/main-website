806e54aaf87891b7571f97cb4cddd5c86d058272
AMendezCerquera <76956375+AMendezCerquera@users.noreply.github.com>
2024-06-27 02:17:00 -0400

    ChatBot Functionalities
    Added chatbot functionalities.
    Updated navigation bar.

--- Structural Elements/script.js
+++ Structural Elements/script.js
@@ -1,43 +1,26 @@
-async function navigateLoad() {
-  try {
-    // Await the fetch operation to complete and then convert response to text
-    const chatBox = await fetch(
-      "/Structural Elements/Loaded Content/Chatbox/index.html"
-    );
+function navigateLoad() {
+  // Access the elements after they have been added to the DOM
+  navigateFunctionalities();
 
-    if (!chatBox.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const chatData = await chatBox.text();
-
-    // Replace the placeholder with the fetched navigation content
-    document.getElementById("chatbot-placeholder").outerHTML = chatData;
-
-    // Access the elements after they have been added to the DOM
-    navigateFunctionalities();
-
-    const swiper = new Swiper(".mySwiper", {
-      direction: "vertical",
-      slidesPerView: 1,
-      spaceBetween: 0,
-      mousewheel: false,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      autoplay: {
-        delay: 10000,
-      },
-    });
-  } catch (error) {
-    console.error("Failed to load navigation:", error);
-  }
+  const swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 0,
+    mousewheel: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 10000,
+    },
+  });
 }
 
 function navigateFunctionalities() {
   const chatbot = document.getElementById("chatbot");
   const navMenu = document.getElementById("nav-menu");
-  const navList = document.getElementById("nav-list");
+  navList = document.getElementById("nav-list");
   const navToggle = document.getElementById("nav-toggle");
   const navClose = document.getElementById("nav-close");
 
@@ -46,17 +29,20 @@ function navigateFunctionalities() {
     navList.classList.toggle("expanded");
     navToggle.style.display = "none";
     navClose.style.display = "flex";
+    hideScrollbar();
   });
 
   navClose.addEventListener("click", () => {
     navList.classList.toggle("expanded");
     navClose.style.display = "none";
     navToggle.style.display = "flex";
+    hideScrollbar();
   });
 
   //Menu Functionalities
   const nevButtons = document.querySelectorAll(".nav__link");
   const contactMenu = document.getElementById("contact-button");
+  chatInvitation = document.getElementById("chat-invitation");
 
   contactMenu.addEventListener("click", () => {
     toggleChat();
@@ -75,56 +61,36 @@ function navigateFunctionalities() {
     chatbot.classList.add("hidden"); // Ensure chatbot is hidden on page load
   });
 
-  const input = document.getElementById("chat-input");
-  input.addEventListener("keypress", function (event) {
-    // If the user presses the "Enter" key on the keyboard
-    if (event.key === "Enter") {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Trigger the button element with a click
-      sendMessage();
-    }
-  });
+  hoverChat();
 }
 
 function toggleChat() {
+  chatInvitation.classList.toggle("hiddens");
+  chatInvitation.classList.toggle("show");
   chatbot.classList.toggle("hidden");
 }
 
-function sendMessage() {
-  const input = document.getElementById("chat-input");
-  const output = document.getElementById("chat-output");
+function hoverChat() {
+  const chatBot = document.querySelector(".chatbot-toggle");
+  const icon1 = document.getElementById("icon1");
+  const icon2 = document.getElementById("icon2");
 
-  const message = input.value.trim();
-  if (message !== "") {
-    output.innerHTML += `<div class='user-message'>${message}</div>`;
-
-    const formData = new URLSearchParams();
-    formData.append("question", message);
-
-    fetch("http://localhost:3000/faq", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        output.innerHTML += `<div class='bot-response'>${data.answer}</div>`;
-        output.scrollTop = output.scrollHeight; // Scroll to the bottom
-      })
-      .catch((error) => console.error("Error:", error));
-
-    input.value = ""; // Clear input after sending
-  }
+  chatBot.addEventListener("mouseover", () => {
+    icon1.classList.add("hidden");
+    icon2.classList.remove("hidden");
+  });
+  chatBot.addEventListener("mouseout", () => {
+    icon2.classList.add("hidden");
+    icon1.classList.remove("hidden");
+  });
 }
 
 var resizeLeft = function resizeLeft() {
   var r = document.getElementById("right-col");
   var l = document.getElementById("left-col");
   var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
-  console.log(width);
   if (width >= 500) {
     l.style.height = r.offsetHeight + "px";
-    console.log(r.offsetHeight);
   }
 };
 
@@ -138,9 +104,214 @@ var addEvent = function (elem, type, eventHandle) {
     elem["on" + type] = eventHandle;
   }
 };
-addEvent(window, "resize", resizeLeft);
 
-resizeLeft();
+function hideScrollbar() {
+  var root = document.getElementsByTagName( 'html' )[0];
+  if (navList.classList.contains('expanded') && window.innerWidth <= 678) {
+      root.classList.add('hidden-scrollbar');
+  } else {
+      root.classList.remove('hidden-scrollbar');
+  }
+}
+
+addEvent(window, "resize", resizeLeft);
+window.addEventListener('resize', hideScrollbar);
+
+//Managing The Prompts for the ChatBox
+const prompts = document.querySelectorAll(".promptButton");
+const messagesContainer = document.getElementById("chat-output");
+
+const initialMessages = [
+  "Hello, I'm Alejandro Bot.",
+  "I'm here to help you with any questions you might have.",
+  "How can i help you today?",
+];
+
+const data = {
+  1: {
+    prompt: ["What are you currently working on?"],
+    messages: ["I'm currently working on a website for an independent consulting company.", "The main focus for the website is to offer healthcare programs to different age groups."],
+    subPrompts: [
+      { subPromptId: "data[1].subPrompts[0].subPrompts[0]", 
+        label: "What stack are you using?",
+        subPrompts: [
+          {
+            messages: [
+              "The customer requested for the website to be a simpler design",
+              "So I'm planning to use HTML, CSS, and JS.",
+              "I mainly want to use JS for any dinamically loaded content that the user wants to use, as well as for a budget calculator."
+            ], subPrompts: [{ subPromptId: "go-back", label: "Go Back" }]
+          }
+        ]
+       },
+      { subPromptId: "go-back", label: "Go Back" },
+    ],
+  },
+  2: {
+    prompt: ["What's on your to-do list for personal projects?"],
+    messages: [
+      "In my future plans, I want to work on:",
+      "A CRM platform for managing a small companies dedicated in healthcare sales.",
+      'A website called "Visit Colombia" with an interactive map to see highlights of my country.',
+    ],
+    subPrompts: [
+      {
+        subPromptId: "data[2].subPrompts[0].subPrompts[0]",
+        label: "What stack are you planning to use?",
+        subPrompts: [
+          {
+            messages: [
+              "So far I've only planned for the CRM.",
+              "For the frontend, I want to use React.js, Redux, and CSS-in-JS",
+              "For Backend, I want to use Node.js, Java, and Express.js",
+              "For the database, I'm not sure yet but either PostgreSQL or MongoDB.",
+              "Last but not least, I want to use RESTful API, Passport.js, and Docker for the remaining parts of the project.",
+            ],
+            subPrompts: [{ subPromptId: "go-back", label: "Go Back" }],
+          },
+        ],
+      },
+      { subPromptId: "go-back", label: "Go Back" },
+    ],
+  },
+  4: {
+    prompt: ["Are you available for hire?"],
+    messages: ["Yes, I am!", "Send me an email below:", "email"],
+    subPrompts: [{ subPromptId: "go-back", label: "Go Back" }],
+  },
+};
+
+function displayInitialMessages() {
+  //Clearing out the space
+  promptsSpace = document.getElementById("prompt-container");
+  promptsSpace.replaceChildren();
+  messagesContainer.replaceChildren();
+
+  //Adding the Initial Messages
+  initialMessages.forEach((message) => {
+    const currentMessage = document.createElement("p");
+    currentMessage.classList.add("bot-message");
+    currentMessage.innerText = message;
+    messagesContainer.appendChild(currentMessage);
+  });
+}
+
+function displayMainPrompts() {
+  promptsSpace = document.getElementById("prompt-container");
+  promptsSpace.replaceChildren();
+  const promptKeys = Object.keys(data);
+  promptKeys.forEach((prompt) => {
+    const currentPrompt = document.createElement("button");
+    currentPrompt.classList.add("promptButton");
+    var currentMessage = data[prompt].prompt;
+    currentPrompt.innerText = currentMessage;
+    var promptId = "data[" + prompt + "]";
+    console.log(promptId);
+    currentPrompt.addEventListener("click", () =>
+      displayContent(promptId, currentMessage)
+    );
+    promptsSpace.appendChild(currentPrompt);
+  });
+  scrollToBottom();
+}
+
+function displayContent(promptId, message) {
+  //messagesContainer.appendChild(promptId);
+  promptsSpace.classList.remove("show");
+  const userSelection = document.createElement("p");
+  userSelection.innerText = message;
+  userSelection.classList.add("user-message");
+  messagesContainer.appendChild(userSelection);
+
+  promptsSpace.replaceChildren();
+  console.log(promptId);
+  const { messages, subPrompts } = eval(promptId);
+
+  // Display messages
+  let delayAmount = 1200;
+  messages.forEach((message, index) => {
+    console.log(message, index);
+    setTimeout(function () {
+      if(message !== "email"){
+        messageElement = document.createElement("p");
+        messageElement.textContent = message;
+      }else {
+        console.log("worked!");
+        messageElement = document.createElement("button");
+        emailContainer = document.createElement("i");
+        emailContainer.classList.add("fa-solid");
+        emailContainer.classList.add("fa-paper-plane");
+        messageElement.classList.add("email-button");
+        emailContainer.addEventListener("click", () => {
+          window.location = "mailto:hello@alejandromendez.dev";
+        })
+        messageElement.appendChild(emailContainer);
+      }
+      messageElement.classList.add("bot-message");
+      messagesContainer.appendChild(messageElement);
+      scrollToBottom();
+      console.log(messagesContainer.offsetHeight);
+      console.log("delay" + delayAmount);
+    }, index * delayAmount); // Multiply the delay by the index
+  });
+
+  // Display sub-prompts
+  setTimeout(function () {
+    if (subPrompts.length > 0) {
+      subPrompts.forEach((subPrompt) => {
+        subPromptButton = document.createElement("button");
+        subPromptButton.className = "subPromptButton";
+        subPromptButton.textContent = subPrompt.label;
+        subPromptButton.setAttribute("data-prompt", subPrompt.subPromptId);
+        if (subPrompt.label === "Go Back") {
+          subPromptButton.addEventListener("click", () => {
+            displayMainPrompts();
+          });
+        } else {
+          subPromptButton.addEventListener("click", () => {
+            displayContent(subPrompt.subPromptId, subPrompt.label);
+          });
+        }
+        promptsSpace.appendChild(subPromptButton);
+      });
+      console.log(delayAmount);
+      promptsSpace.classList.toggle("show");
+      scrollToBottom();
+    }
+  }, messages.length * delayAmount);
+}
+
+function waitForCondition(conditionFn, interval = 100) {
+  return new Promise((resolve) => {
+    const intervalId = setInterval(() => {
+      if (conditionFn()) {
+        clearInterval(intervalId);
+        resolve();
+      }
+    }, interval);
+  });
+}
+
+chatMessageWindow = document.querySelector(".chat-messages");
+
+function scrollToBottom() {
+  //console.log("messagesContainer Height: "+messagesContainer.offsetHeight);
+  //chatMessageWindow.scrollTop = messagesContainer.scrollHeight;
+  //console.log(chatMessageWindow.scrollTop);
+  const lastMessage = messagesContainer.lastElementChild;
+  if (lastMessage) {
+    lastMessage.scrollIntoView({behavior: 'smooth'});
+  }
+}
+
+/*
+function scrollToBottom() {
+  chatMessageWindow.scrollTop = messagesContainer.scrollHeight;
+}
+*/
 
 // Call the navigateLoad function on window load
 window.onload = navigateLoad();
+resizeLeft();
+displayInitialMessages();
+displayMainPrompts();
